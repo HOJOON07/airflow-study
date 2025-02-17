53142bebae1ae3e4a8ffd6039f14b4365da620a6
김호준 <gimhojun@gimhojuns-MacBook-Pro.local>
2025-02-17 23:56:11 +0900

    module import function operator

---
+++ airflow_local_settings.py
@@ -0,0 +1,14 @@
+# STATE_COLORS = {
+#     "deferred": "mediumpurple",
+#     "failed": "firebrick",
+#     "queued": "darkgray",
+#     "removed": "lightgrey",
+#     "restarting": "violet",
+#     "running": "#01FF70",
+#     "scheduled": "tan",
+#     "skipped": "darkorchid",
+#     "success": "#2ECC40",
+#     "up_for_reschedule": "turquoise",
+#     "up_for_retry": "yellow",
+#     "upstream_failed": "orange",
+# }
