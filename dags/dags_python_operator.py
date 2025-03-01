d19d99a160471c91c76f33dd844a0664520099b5
hojoon07 <ghwns107@naver.com>
2025-03-01 20:49:29 +0900

    airflow template operator

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -8,16 +8,14 @@ with DAG(
     dag_id="dags_python_operator",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2025, 2, 16, tz="Asia/Seoul"),
-    catchup=False
+    catchup=False,
 ) as dag:
+
     def select_fruit():
         fruit = ["APPLE", "BANANA", "ORANGE", "AVOCADO"]
         rand_int = random.randint(0, 3)
         print(fruit[rand_int])
 
-    py_t1 = PythonOperator(
-        task_id="py_t1",
-        python_callable=select_fruit
-    )
+    py_t1 = PythonOperator(task_id="py_t1", python_callable=select_fruit)
 
-    py_t1
+    py_t1()
